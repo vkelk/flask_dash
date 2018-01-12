f96753b853076efaf202f77d5799c88f53a2015d
Vasko Kelkocev <pisimina@gmail.com>
2018-01-12 15:54:17 +0100

    event case

--- application/fintweet/forms.py
+++ application/fintweet/forms.py
@@ -1,5 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, DateField, RadioField
+from wtforms import StringField, SelectField, DateField, RadioField, FileField, FormField
+
+
+class Form2(FlaskForm):
+    company_codes = StringField()
+    codes_file = FileField()
 
 
 class Form1(FlaskForm):
@@ -10,6 +15,7 @@ class Form1(FlaskForm):
                                  choices=[('permno', 'PermNo'), ('ticker', 'Ticker'), ('hashtag', 'Hashtag'),
                                           ('cashtag', 'Cashtag'), ('mentions', 'Mentions'),
                                           ('user_names', 'User Names')])
+    codes_container_radio = FormField(Form2)
 
     permno = SelectField()
     cashtag = SelectField()

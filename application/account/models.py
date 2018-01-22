4787f9255704791faec30fb0de99fec377cfb8a5
Vasko Kelkocev <pisimina@gmail.com>
2018-01-22 07:29:31 +0100

    event study file

--- application/account/models.py
+++ application/account/models.py
@@ -23,7 +23,17 @@ class Account(UserMixin, db.Model):
 
     # __table__ = db.Model.metadata.tables['accounts']
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password = db.Column(db.Binary(60), nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+    email_confirmation_sent_on = db.Column(db.DateTime, nullable=True)
+    email_confirmed = db.Column(db.Boolean, nullable=True, default=False)
+    email_confirmed_on = db.Column(db.DateTime, nullable=True)
+    registered_on = db.Column(db.DateTime, nullable=True)
+    last_logged_in = db.Column(db.DateTime, nullable=True)
+    current_logged_in = db.Column(db.DateTime, nullable=True)
+    role = db.Column(db.String, default='user')
 
     def __init__(self, email, password, email_confirmation_sent_on=None, role='user'):
         self.email = email

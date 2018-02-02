b1a0fab2257810d88b2bff54bda6b1170aa515a0
Vasko Kelkocev <pisimina@gmail.com>
2018-02-02 15:34:10 +0100

    datatables and charts

---
+++ migration_tools/update_users_mydrive.py
@@ -0,0 +1,119 @@
+import concurrent.futures as cf
+import MySQLdb
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from pprint import pprint
+from datetime import datetime
+
+PG_DBNAME = 'tweets'
+PG_USER = 'postgres'
+PG_PASSWORD = ''
+MY_DBNAME = 'usrtweet'
+MY_USER = 'root'
+MY_PASWORD = 'iIe8jrzYvmB1dxQbbCR9'
+DB_HOST = 'localhost'
+
+my_config = {'username': MY_USER, 'password': MY_PASWORD, 'database': MY_DBNAME, 'host': DB_HOST}
+pg_config = {'username': PG_USER, 'password': PG_PASSWORD, 'database': PG_DBNAME, 'host': DB_HOST}
+
+mydrive = "mysql://{username}:{password}@{host}:3306/mydrive55".format(**my_config)
+pg_dsn = "postgresql+psycopg2://{username}@{host}:5432/{database}".format(**pg_config)
+
+Base = declarative_base()
+src = create_engine(mydrive)
+dst = create_engine(pg_dsn)
+pg_meta = MetaData(bind=dst, schema="fintweet")
+my_meta = MetaData(bind=src)
+
+
+# Reflect destination tables
+
+class MyUsers(Base):
+    __table__ = Table('user', my_meta, autoload=True)
+
+
+class MyUsersCount(Base):
+    __table__ = Table('user_count', my_meta, autoload=True)
+
+
+class PgUsers(Base):
+    __table__ = Table('user', pg_meta, autoload=True)
+
+
+class PgUsersCount(Base):
+    __table__ = Table('user_count', pg_meta, autoload=True)
+
+
+# Create a session to use the tables
+SrcSession = sessionmaker(bind=src)
+session_factory = sessionmaker(dst, autocommit=True, autoflush=True)
+# DstSession = scoped_session(session_factory)
+DstSession = sessionmaker(bind=dst)
+
+srcssn = SrcSession()
+dstssn = DstSession()
+
+
+def update_user(row):
+    try:
+        dstssn = DstSession()
+        print("Inserting user:", row.UserScreenName)
+        item = PgUsers(
+            user_id=row.user_id,
+            twitter_handle=row.UserScreenName,
+            location=row.location,
+            date_joined=row.date_joined,
+            website=row.website,
+            verified=row.is_verified
+        )
+        dstssn.add(item)
+        dstssn.commit()
+
+    except BaseException as e:
+        print(str(e))
+        raise
+
+
+def update_user_count(row):
+    try:
+        dstssn = DstSession()
+        print("Inserting count for user_id:", row.user_id)
+        item = PgUsersCount(
+            user_id=row.user_id,
+            follower=row.FollowersCount,
+            following=row.FollowingCount,
+            tweets=row.TweetsCount,
+            likes=row.LikesCount
+        )
+        dstssn.add(item)
+        dstssn.commit()
+
+    except BaseException as e:
+        print(str(e))
+        raise
+
+
+if __name__ == '__main__':
+    for my_user in srcssn.query(MyUsers).execution_options(stream_results=True).yield_per(200):
+        pg_user = dstssn.query(PgUsers).filter_by(user_id=my_user.user_id).first()
+        uflag = False
+        if pg_user.location is None and my_user.location:
+            pg_user.location = my_user.location
+            uflag = True
+        if pg_user.date_joined is None and my_user.date_joined:
+            pg_user.date_joined = my_user.date_joined
+            uflag = True
+        if pg_user.website is None and my_user.website:
+            pg_user.website = my_user.website
+            uflag = True
+        if uflag:
+            print("Updating user_id", pg_user.user_id)
+            dstssn.commit()
+
+    # for my_user_count in srcssn.query(MyUsersCount).execution_options(stream_results=True).yield_per(200):
+    #     pg_user_count = dstssn.query(PgUsersCount).filter_by(user_id=my_user_count.user_id).first()
+    #     if pg_user_count is None:
+    #         update_user_count(my_user_count)
+
+    print("ALL DONE.")

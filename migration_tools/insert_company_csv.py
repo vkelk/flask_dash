212a9017262f21e3fff5e092e5ee4c5ec5b57494
Vasko Kelkocev <pisimina@gmail.com>
2018-03-21 00:25:09 +0100

    company_ids

--- migration_tools/insert_company_csv.py
+++ migration_tools/insert_company_csv.py
@@ -1,9 +1,10 @@
 import concurrent.futures as cf
 import csv
-import logging 
+import logging
 import logging.config
 import os
 import re
+import sys
 import time
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -18,8 +19,30 @@ CSV_DIR = os.path.join(os.path.dirname(DIR_PATH), 'files/company_ids')
 
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
-pg_dsn = "postgresql+psycopg2://{username}:{password}@{host}:5432/{database}".format(**pg_config)
 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+regex_str = [
+    # emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    # r"(?:\$+[a-zA-Z]+[\w\'_\-]*[\w_]+)",  # cash-tags
+    r"(\$[a-zA-Z]{1,7}(?:[.:]{1}[a-zA-Z]{1,7})?\b)",  # cash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+pg_dsn = "postgresql+psycopg2://{username}:{password}@{host}:5432/{database}".format(**pg_config)
 Base = declarative_base()
 db_engine = create_engine(pg_dsn, pool_size=100, max_overflow=0)
 db_meta = MetaData(bind=db_engine, schema="fintweet")
@@ -27,36 +50,174 @@ session_factory = sessionmaker(db_engine, autocommit=True, autoflush=True)
 Session = scoped_session(session_factory)
 
 
-class PgUsers(Base):
+# Database definition clases
+class User(Base):
     __table__ = Table('user', db_meta, autoload=True)
 
 
-class PgUsersCount(Base):
+class UserCount(Base):
     __table__ = Table('user_count', db_meta, autoload=True)
 
 
-class PgTweets(Base):
+class Tweet(Base):
     __table__ = Table('tweet', db_meta, autoload=True)
 
 
-class PgTweetCounts(Base):
+class TweetCount(Base):
     __table__ = Table('tweet_count', db_meta, autoload=True)
 
 
-class PgTweetCashtags(Base):
+class TweetCashtag(Base):
     __table__ = Table('tweet_cashtags', db_meta, autoload=True)
 
 
-class PgTweetHashtags(Base):
+class TweetHashtag(Base):
     __table__ = Table('tweet_hashtags', db_meta, autoload=True)
 
 
-class PgTweetMentions(Base):
+class TweetMentions(Base):
     __table__ = Table('tweet_mentions', db_meta, autoload=True)
 
 
-class PgTweetUrls(Base):
+class TweetUrls(Base):
     __table__ = Table('tweet_url', db_meta, autoload=True)
+
+
+def fname_exists(fname):
+    session = Session()
+    file_db = session.query(FileInfo).filter_by(filename=fname).first()
+    Session.remove()
+    if file_db is None:
+        return False
+    elif file_db.status == 'finished':
+        return True
+    return False
+
+
+def add_user(row):
+    user = User(
+        user_id=row['UserID'],
+        twitter_handle=row['UserScreenName'],
+        user_name=row['UserName'],
+        verified=row['isVerified'],
+        is_company=True
+    )
+    return user
+
+
+def add_user_count(row):
+    FollowersCount = to_integer(row['UserFollowersCount'])
+    FollowingCount = to_integer(row['UserFollowingCount'])
+    TweetsCount = to_integer(row['UserTweetsCount'])
+    # LikesCount = to_integer(row['FavoriteCount'])
+    user_count = UserCount(
+        user_id=row['UserID'],
+        follower=FollowersCount,
+        following=FollowingCount,
+        tweets=TweetsCount
+        # likes=LikesCount
+    )
+    return user_count
+
+
+def add_tweet(row):
+    try:
+        date = datetime.strptime(row['Date'], '%d/%m/%Y')
+    except BaseException as e:
+        logger.warning(str(e))
+        date = None
+    tweet = Tweet(
+        tweet_id=row['TweetID'],
+        date=date,
+        time=row['Time'],
+        retweet_status=row['isRetweet'],
+        text=row['Text'],
+        user_id=row['UserID'],
+        reply_to=row['ReplyTweetID'],
+        permalink=row['Permalink']
+    )
+    return tweet
+
+
+def add_tweet_count(row):
+    Replies = to_integer(row['ReplyesCount'])
+    Retweets = to_integer(row['RetweetCount'])
+    Favorites = to_integer(row['FavoriteCount'])
+    tweet_count = TweetCount(
+        tweet_id=row['TweetID'],
+        reply=Replies,
+        retweet=Retweets,
+        favorite=Favorites
+    )
+    return tweet_count
+
+
+def add_tweet_cashtag(row, cashtag):
+    tweet_cashtag = TweetCashtag(
+        tweet_id=row['TweetID'],
+        cashtags=cashtag
+    )
+    return tweet_cashtag
+
+
+def add_tweet_hashtag(row, hashtag):
+    tweet_hashtag = TweetHashtag(
+        tweet_id=row['TweetID'],
+        hashtags=hashtag
+    )
+    return tweet_hashtag
+
+
+def add_tweet_mention(row, mention):
+    mentions, mentions_id = mention
+    tweet_mention = TweetMentions(
+        tweet_id=row['TweetID'],
+        mentions=mentions,
+        user_id=mentions_id
+    )
+    return tweet_mention
+
+
+def add_tweet_url(row, url):
+    if len(url) > 255:
+        url = url[:255]
+    tweet_url = TweetUrls(
+        tweet_id=row['TweetID'],
+        url=url
+    )
+    return tweet_url
+
+
+def add_file_info(fname, row, count):
+    try:
+        date_start = datetime.strptime(row['QueryStartDate'], '%Y-%m-%d')
+    except BaseException as e:
+        try:
+            date_start = datetime.strptime(row['QueryStartDate'], '%m/%d/%Y')
+        except BaseException as e:
+            logger.warning(str(e))
+            date_start = None
+    try:
+        date_end = datetime.strptime(row['QueryEndDate'], '%Y-%m-%d')
+    except BaseException as e:
+        try:
+            date_end = datetime.strptime(row['QueryEndDate'], '%m/%d/%Y')
+        except BaseException as e:
+            logger.warning(str(e))
+            date_end = None
+        # date_end = None
+    file_info = FileInfo(
+        filename=fname,
+        fileloc=os.path.join(CSV_DIR, fname),
+        permno=row['permno'],
+        query=row['Query'],
+        keyword=row['Keyword'],
+        query_date_start=date_start,
+        query_date_end=date_end,
+        status='working',
+        last_line=count
+    )
+    return file_info
 
 
 def create_logger(dirname, filename):
@@ -66,6 +227,52 @@ def create_logger(dirname, filename):
     log_file = log_file.replace('\\', '/')
     logging.config.fileConfig('log.ini', defaults={'logfilename': log_file})
     return logging.getLogger(__name__)
+
+
+def to_integer(string):
+    try:
+        return int(string)
+    except ValueError:
+        return 0
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preprocess(s, lowercase=False, upercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    if upercase:
+        tokens = [token.upper() for token in tokens]
+    return tokens
+
+
+def process_tweet(row):
+    session = Session()
+    session.add(add_tweet(row))
+    session.add(add_tweet_count(row))
+    tokens = preprocess(row['Text'])
+    cashtags = [term for term in tokens if term.startswith('$') and len(term) > 1]
+    hashtags = [term for term in tokens if term.startswith('#') and len(term) > 1]
+    # mentions = [term for term in tokens if term.startswith('@') and len(term) > 1]
+    urls = [term for term in tokens if term.startswith('http') and len(term) > 4]
+    if len(cashtags) > 0:
+        for cashtag in cashtags:
+            session.add(add_tweet_cashtag(row, cashtag.upper().strip()))
+    if len(hashtags) > 0:
+        for hashtag in hashtags:
+            session.add(add_tweet_hashtag(row, hashtag.strip()))
+    if len(urls) > 0:
+        for url in urls:
+            session.add(add_tweet_url(row, url.strip()))
+    if len(row['Mentions_name'].strip()) > 0:
+        mentions = row['Mentions_name'].splitlines()
+        mentions_ids = row['Mentions_id'].splitlines()
+        ment = zip(mentions, mentions_ids)
+        for mention in ment:
+            session.add(add_tweet_mention(row, mention))
 
 
 def process_file(fname):
@@ -113,42 +320,13 @@ def process_file(fname):
                 tweet_db = session.query(Tweet).filter_by(tweet_id=row['TweetID']).first()
                 # permno_db = session.query(PermNo).filter_by(permno=row['permno']).first()
                 if not tweet_db:
-                    session.add(add_tweet(row))
-                    session.add(add_tweet_count(row))
-                    if len(row['Symbols'].strip()) > 0:
-                        cashtags = row['Symbols'].split('\n')
-                        for cashtag in cashtags:
-                            cashtag = cashtag.strip()
-                            session.add(add_tweet_cashtag(row, cashtag))
-                    if len(row['Hashtags'].strip()) > 0:
-                        hashtags = row['Hashtags'].split(' ')
-                        for hashtag in hashtags:
-                            hashtag = hashtag.strip()
-                            session.add(add_tweet_hashtag(row, hashtag))
+                    process_tweet(row)
                     if len(row['Mentions_name'].strip()) > 0:
                         mentions = row['Mentions_name'].splitlines()
                         mentions_ids = row['Mentions_id'].splitlines()
                         ment = zip(mentions, mentions_ids)
                         for mention in ment:
                             session.add(add_tweet_mention(row, mention))
-                    if len(row['Urls'].strip()) > 0:
-                        urls = row['Urls'].split(',')
-                        for url in urls:
-                            url = url.strip()
-                            session.add(add_tweet_url(row, url))
-                    # session.add(add_permno(row))
-                # elif not permno_db:
-                #     if len(row['CashtagSymbols'].strip()) > 0:
-                #         cashtags = row['CashtagSymbols'].split('\n')
-                #         for cashtag in cashtags:
-                #             cashtag = cashtag.strip()
-                #             session.add(add_tweet_cashtag(row, cashtag))
-                #     if len(row['Hashtags'].strip()) > 0:
-                #         hashtags = row['Hashtags'].split(' ')
-                #         for hashtag in hashtags:
-                #             hashtag = hashtag.strip()
-                #             session.add(add_tweet_hashtag(row, hashtag))
-                #     session.add(add_permno(row))
             except BaseException as e:
                 logger.error(str(e))
                 raise

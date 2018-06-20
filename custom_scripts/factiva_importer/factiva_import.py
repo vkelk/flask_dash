670739b4926da6bba81db5911bbb467bddd31eab
Vasko Kelkocev <pisimina@gmail.com>
2018-06-20 20:23:33 +0200

    hyperlink fixed

--- custom_scripts/factiva_importer/factiva_import.py
+++ custom_scripts/factiva_importer/factiva_import.py
@@ -6,45 +6,45 @@ def striprtf(text):
     pattern = re.compile(r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)", re.I)
     # control words which specify a "destionation".
     destinations = frozenset((
-        'aftncn','aftnsep','aftnsepc','annotation','atnauthor','atndate','atnicn','atnid',
-        'atnparent','atnref','atntime','atrfend','atrfstart','author','background',
-        'bkmkend','bkmkstart','blipuid','buptim','category','colorschememapping',
-        'colortbl','comment','company','creatim','datafield','datastore','defchp','defpap',
-        'do','doccomm','docvar','dptxbxtext','ebcend','ebcstart','factoidname','falt',
-        'fchars','ffdeftext','ffentrymcr','ffexitmcr','ffformat','ffhelptext','ffl',
-        'ffname','ffstattext','field','file','filetbl','fldinst','fldrslt','fldtype',
-        'fname','fontemb','fontfile','fonttbl','footer','footerf','footerl','footerr',
-        'footnote','formfield','ftncn','ftnsep','ftnsepc','g','generator','gridtbl',
-        'header','headerf','headerl','headerr','hl','hlfr','hlinkbase','hlloc','hlsrc',
-        'hsv','htmltag','info','keycode','keywords','latentstyles','lchars','levelnumbers',
-        'leveltext','lfolevel','linkval','list','listlevel','listname','listoverride',
-        'listoverridetable','listpicture','liststylename','listtable','listtext',
-        'lsdlockedexcept','macc','maccPr','mailmerge','maln','malnScr','manager','margPr',
-        'mbar','mbarPr','mbaseJc','mbegChr','mborderBox','mborderBoxPr','mbox','mboxPr',
-        'mchr','mcount','mctrlPr','md','mdeg','mdegHide','mden','mdiff','mdPr','me',
-        'mendChr','meqArr','meqArrPr','mf','mfName','mfPr','mfunc','mfuncPr','mgroupChr',
-        'mgroupChrPr','mgrow','mhideBot','mhideLeft','mhideRight','mhideTop','mhtmltag',
-        'mlim','mlimloc','mlimlow','mlimlowPr','mlimupp','mlimuppPr','mm','mmaddfieldname',
-        'mmath','mmathPict','mmathPr','mmaxdist','mmc','mmcJc','mmconnectstr',
-        'mmconnectstrdata','mmcPr','mmcs','mmdatasource','mmheadersource','mmmailsubject',
-        'mmodso','mmodsofilter','mmodsofldmpdata','mmodsomappedname','mmodsoname',
-        'mmodsorecipdata','mmodsosort','mmodsosrc','mmodsotable','mmodsoudl',
-        'mmodsoudldata','mmodsouniquetag','mmPr','mmquery','mmr','mnary','mnaryPr',
-        'mnoBreak','mnum','mobjDist','moMath','moMathPara','moMathParaPr','mopEmu',
-        'mphant','mphantPr','mplcHide','mpos','mr','mrad','mradPr','mrPr','msepChr',
-        'mshow','mshp','msPre','msPrePr','msSub','msSubPr','msSubSup','msSubSupPr','msSup',
-        'msSupPr','mstrikeBLTR','mstrikeH','mstrikeTLBR','mstrikeV','msub','msubHide',
-        'msup','msupHide','mtransp','mtype','mvertJc','mvfmf','mvfml','mvtof','mvtol',
-        'mzeroAsc','mzeroDesc','mzeroWid','nesttableprops','nextfile','nonesttables',
-        'objalias','objclass','objdata','object','objname','objsect','objtime','oldcprops',
-        'oldpprops','oldsprops','oldtprops','oleclsid','operator','panose','password',
-        'passwordhash','pgp','pgptbl','picprop','pict','pn','pnseclvl','pntext','pntxta',
-        'pntxtb','printim','private','propname','protend','protstart','protusertbl','pxe',
-        'result','revtbl','revtim','rsidtbl','rxe','shp','shpgrp','shpinst',
-        'shppict','shprslt','shptxt','sn','sp','staticval','stylesheet','subject','sv',
-        'svb','tc','template','themedata','title','txe','ud','upr','userprops',
-        'wgrffmtfilter','windowcaption','writereservation','writereservhash','xe','xform',
-        'xmlattrname','xmlattrvalue','xmlclose','xmlname','xmlnstbl',
+        'aftncn', 'aftnsep', 'aftnsepc', 'annotation', 'atnauthor', 'atndate', 'atnicn', 'atnid',
+        'atnparent', 'atnref', 'atntime', 'atrfend', 'atrfstart', 'author', 'background',
+        'bkmkend', 'bkmkstart', 'blipuid', 'buptim', 'category', 'colorschememapping',
+        'colortbl', 'comment', 'company', 'creatim', 'datafield', 'datastore', 'defchp', 'defpap',
+        'do', 'doccomm', 'docvar', 'dptxbxtext', 'ebcend', 'ebcstart', 'factoidname', 'falt',
+        'fchars', 'ffdeftext', 'ffentrymcr', 'ffexitmcr', 'ffformat', 'ffhelptext', 'ffl',
+        'ffname', 'ffstattext', 'file', 'filetbl', 'fldtype',
+        'fname', 'fontemb', 'fontfile', 'fonttbl', 'footer', 'footerf', 'footerl', 'footerr',
+        'footnote', 'formfield', 'ftncn', 'ftnsep', 'ftnsepc', 'g', 'generator', 'gridtbl',
+        'header', 'headerf', 'headerl', 'headerr', 'hl', 'hlfr', 'hlinkbase', 'hlloc', 'hlsrc',
+        'hsv', 'htmltag', 'info', 'keycode', 'keywords', 'latentstyles', 'lchars', 'levelnumbers',
+        'leveltext', 'lfolevel', 'linkval', 'list', 'listlevel', 'listname', 'listoverride',
+        'listoverridetable', 'listpicture', 'liststylename', 'listtable', 'listtext',
+        'lsdlockedexcept', 'macc', 'maccPr', 'mailmerge', 'maln', 'malnScr', 'manager', 'margPr',
+        'mbar', 'mbarPr', 'mbaseJc', 'mbegChr', 'mborderBox', 'mborderBoxPr', 'mbox', 'mboxPr',
+        'mchr', 'mcount', 'mctrlPr', 'md', 'mdeg', 'mdegHide', 'mden', 'mdiff', 'mdPr', 'me',
+        'mendChr', 'meqArr', 'meqArrPr', 'mf', 'mfName', 'mfPr', 'mfunc', 'mfuncPr', 'mgroupChr',
+        'mgroupChrPr', 'mgrow', 'mhideBot', 'mhideLeft', 'mhideRight', 'mhideTop', 'mhtmltag',
+        'mlim', 'mlimloc', 'mlimlow', 'mlimlowPr', 'mlimupp', 'mlimuppPr', 'mm', 'mmaddfieldname',
+        'mmath', 'mmathPict', 'mmathPr', 'mmaxdist', 'mmc', 'mmcJc', 'mmconnectstr',
+        'mmconnectstrdata', 'mmcPr', 'mmcs', 'mmdatasource', 'mmheadersource', 'mmmailsubject',
+        'mmodso', 'mmodsofilter', 'mmodsofldmpdata', 'mmodsomappedname', 'mmodsoname',
+        'mmodsorecipdata', 'mmodsosort', 'mmodsosrc', 'mmodsotable', 'mmodsoudl',
+        'mmodsoudldata', 'mmodsouniquetag', 'mmPr', 'mmquery', 'mmr', 'mnary', 'mnaryPr',
+        'mnoBreak', 'mnum', 'mobjDist', 'moMath', 'moMathPara', 'moMathParaPr', 'mopEmu',
+        'mphant', 'mphantPr', 'mplcHide', 'mpos', 'mr', 'mrad', 'mradPr', 'mrPr', 'msepChr',
+        'mshow', 'mshp', 'msPre', 'msPrePr', 'msSub', 'msSubPr', 'msSubSup', 'msSubSupPr', 'msSup',
+        'msSupPr', 'mstrikeBLTR', 'mstrikeH', 'mstrikeTLBR', 'mstrikeV', 'msub', 'msubHide',
+        'msup', 'msupHide', 'mtransp', 'mtype', 'mvertJc', 'mvfmf', 'mvfml', 'mvtof', 'mvtol',
+        'mzeroAsc', 'mzeroDesc', 'mzeroWid', 'nesttableprops', 'nextfile', 'nonesttables',
+        'objalias', 'objclass', 'objdata', 'object', 'objname', 'objsect', 'objtime', 'oldcprops',
+        'oldpprops', 'oldsprops', 'oldtprops', 'oleclsid', 'operator', 'panose', 'password',
+        'passwordhash', 'pgp', 'pgptbl', 'picprop', 'pict', 'pn', 'pnseclvl', 'pntext', 'pntxta',
+        'pntxtb', 'printim', 'private', 'propname', 'protend', 'protstart', 'protusertbl', 'pxe',
+        'result', 'revtbl', 'revtim', 'rsidtbl', 'rxe', 'shp', 'shpgrp', 'shpinst',
+        'shppict', 'shprslt', 'shptxt', 'sn', 'sp', 'staticval', 'stylesheet', 'subject', 'sv',
+        'svb', 'tc', 'template', 'themedata', 'title', 'txe', 'ud', 'upr', 'userprops',
+        'wgrffmtfilter', 'windowcaption', 'writereservation', 'writereservhash', 'xe', 'xform',
+        'xmlattrname', 'xmlattrvalue', 'xmlclose', 'xmlname', 'xmlnstbl',
         'xmlopen',
     ))
     # Translation of some special characters.
@@ -73,16 +73,16 @@ def striprtf(text):
     curskip = 0             # Number of ASCII characters left to skip
     out = []                # Output buffer.
     for match in pattern.finditer(text.decode()):
-        word,arg,hex,char,brace,tchar = match.groups()
+        word, arg, hex, char, brace, tchar = match.groups()
         if brace:
             curskip = 0
             if brace == '{':
                 # Push state
-                stack.append((ucskip,ignorable))
+                stack.append((ucskip, ignorable))
             elif brace == '}':
                 # Pop state
-                ucskip,ignorable = stack.pop()
-        elif char: # \x (not a letter)
+                ucskip, ignorable = stack.pop()
+        elif char:  # \x (not a letter)
             curskip = 0
             if char == '~':
                 if not ignorable:
@@ -92,7 +92,7 @@ def striprtf(text):
                     out.append(char)
             elif char == '*':
                 ignorable = True
-        elif word: # \foo
+        elif word:  # \foo
             curskip = 0
             if word in destinations:
                 ignorable = True
@@ -104,23 +104,24 @@ def striprtf(text):
                 ucskip = int(arg)
             elif word == 'u':
                 c = int(arg)
-                if c < 0: c += 0x10000
-                if c > 127: out.append(chr(c)) #NOQA
-                else: out.append(chr(c))
+                if c < 0: c += 0x10000  # NOQA
+                if c > 127: out.append(chr(c))  # NOQA
+                else: out.append(chr(c))  # NOQA
                 curskip = ucskip
-        elif hex: # \'xx
+        elif hex:  # \'xx
             if curskip > 0:
                 curskip -= 1
             elif not ignorable:
-                c = int(hex,16)
-                if c > 127: out.append(chr(c)) #NOQA
-                else: out.append(chr(c))
+                c = int(hex, 16)
+                if c > 127: out.append(chr(c))  # NOQA
+                else: out.append(chr(c))  # NOQA
         elif tchar:
             if curskip > 0:
                 curskip -= 1
             elif not ignorable:
                 out.append(tchar)
     return ''.join(out)
+
 
 def parser(data):
     articles = []
@@ -131,14 +132,14 @@ def parser(data):
         a = '\t' + a
         articles.append(a)
         start = end
- 
+
     # In each article, find all used Intelligence Indexing field codes. Extract
     # content of each used field code, and write to a CSV file.
- 
+
     # All field codes (order matters)
     fields = ['HD', 'CR', 'WC', 'PD', 'ET', 'SN', 'SC', 'ED', 'PG', 'LA', 'CY', 'LP',
               'TD', 'CT', 'RF', 'CO', 'IN', 'NS', 'RE', 'IPC', 'IPD', 'PUB', 'AN']
- 
+
     for a in articles:
         used = [f for f in fields if re.search(r'\t' + f + r'\t', a)]
         unused = [[i, f] for i, f in enumerate(fields) if not re.search(r'\t' + f + r'\t', a)]
@@ -160,21 +161,14 @@ def parser(data):
             obs.append(content)
         for f in unused:
             obs.insert(f[0], '')
-        # obs.insert(0, file.split('/')[-1].split('.')[0])  # insert Company ID, e.g., GVKEY
-        print(obs)
-        dictionary = dict(zip(fields, obs))
-        pprint(dictionary)
-        exit()
-        # cur.execute('''INSERT INTO articles
-        #                (id, hd, cr, wc, pd, et, sn, sc, ed, pg, la, cy, lp, td, ct, rf,
-        #                co, ina, ns, re, ipc, ipd, pub, an)
-        #                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-        #                ?, ?, ?, ?, ?, ?, ?, ?)''', obs)
+        raw_dict = dict(zip(fields, obs))
+        return raw_dict
+
 
 raw_data_folder = "rtfs/"
 file1 = open(raw_data_folder + 'Factiva-20180619-1930.rtf', 'rb')
 txt = file1.read()
 # print(striprtf(txt))
-with open('test.txt', 'w') as f: 
-    f.write(striprtf(txt)) 
-parser(striprtf(txt))
+# with open('test.txt', 'w') as f:
+#     f.write(striprtf(txt))
+print(parser(striprtf(txt)))

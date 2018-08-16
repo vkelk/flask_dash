1b00410a6b4d2354acb900eb9ec25afecf023705
Vasko Kelkocev <pisimina@gmail.com>
2018-08-16 15:14:54 +0200

    mat view for stocktwits cashtags

--- custom_scripts/counts/mat_views.sql
+++ custom_scripts/counts/mat_views.sql
@@ -15,3 +15,14 @@ SELECT row_number() OVER (ORDER BY t.tweet_id) AS id,
 
 ALTER MATERIALIZED VIEW "fintweet"."mv_cashtags" OWNER TO "postgres";
 
+CREATE MATERIALIZED VIEW "stocktwits"."mv_cashtags"
+AS
+SELECT row_number() OVER (ORDER BY i.ideas_id) AS id,
+    i.ideas_id,
+    i.user_id,
+    c.cashtag,
+    timezone('America/New_York'::text, timezone('UTC'::text, i.datetime)) AS datetime
+   FROM (stocktwits.idea_cashtags c
+     JOIN stocktwits.ideas i ON ((i.ideas_id = c.ideas_id)))
+  WHERE ((i.datetime >= '2012-01-01'::date) AND (i.datetime <= '2017-01-01'::date))
+  ORDER BY i.ideas_id;
